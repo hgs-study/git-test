f9baa20a98eac31c5b8a6b90ca35b879fc4066b7
hgs-study <hgstudy_@naver.com>
2021-11-30 21:31:20 +0900

    add commit feature/2

--- src/main/java/com/gittest/feature02.java
+++ src/main/java/com/gittest/feature02.java
@@ -4,5 +4,6 @@ public class feature02 {
     public static void main(String[] args) {
         System.out.println("feature02");
         System.out.println("add commit feature02");
+        System.out.println("add commit feature02");
     }
 }
