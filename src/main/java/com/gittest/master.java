8a525d06bbe17c20d4c7b043eb68ebd40d672da6
hgs-study <hgstudy_@naver.com>
2021-11-30 21:57:19 +0900

    add commit master

--- src/main/java/com/gittest/master.java
+++ src/main/java/com/gittest/master.java
@@ -3,5 +3,6 @@ package com.gittest;
 public class master {
     public static void main(String[] args) {
         System.out.println("master");
+        System.out.println("add commit master");
     }
 }
