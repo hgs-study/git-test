78a0bddb7cf6a0ed47f16c4b5c918d8ad3c97148
hgs-study <hgstudy_@naver.com>
2021-11-30 22:08:21 +0900

    squash test1

--- src/main/java/com/gittest/feature3.java
+++ src/main/java/com/gittest/feature3.java
@@ -10,5 +10,6 @@ public class feature3 {
         System.out.println("rebase test");
         System.out.println("rebase test2");
         System.out.println("rebase test3");
+        System.out.println("squash test1");
     }
 }
